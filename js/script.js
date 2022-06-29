06bbde2dbb05ee9195e38b151395692d40fa10d7
Michal Frankiewicz <michal_frankiewicz@outlook.com>
2022-06-29 14:59:37 +0200

    Add changes in script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,60 @@ function printMessage(msg) {
 function clearMessages() {
     document.getElementById('messages').innerHTML = ''
 }
-let playerMove = `papier`
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
+
+console.log('Gracz wpisał: ' + playerInput)
+
+let playerMove = 'nieznany ruch'
+
+if (playerInput == '1') {
+    playerMove = 'kamień'
+} else if (playerInput == '2') {
+    playerMove = 'papier'
+} else if (playerInput == '3') {
+    playerMove = 'nożyce'
+} else {
+    playerMove = 'nieznany ruch'
+}
+
+printMessage('Twój ruch to: ' + playerMove)
+
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+console.log('Wylosowana liczba to: ' + randomNumber)
+
+let computerMove = 'nieznany ruch'
+
+if (randomNumber == '1') {
+    computerMove = 'kamień'
+} else if (randomNumber == '2') {
+    computerMove = 'papier'
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce'
+} else {
+    computerMove = 'nieznany ruch'
+}
+
+printMessage('Ruch komputera to : ' + computerMove)
+
+if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis')
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis')
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis')
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz')
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Wygrywa komputer')
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz')
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Wygrywa komputer')
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Wygrywa komputer')
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz')
+} else if (computerMove == 'nieznany ruch' || playerMove == 'nieznany ruch') {
+    printMessage('Błąd gry')
+}
